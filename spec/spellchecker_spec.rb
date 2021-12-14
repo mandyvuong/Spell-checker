accf3eb0e58239877f2ed9d4a2c93c1e3dc46a41
amanda vuong <mandyvuong@gmail.com>
2021-12-14 13:13:43 +0000

    returns the corrected single word (upcase)

--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -7,5 +7,8 @@ describe 'spellchecker' do
   it 'returns the corrected single word (capitalize)' do
     expect(spellchecker('At')).to eq 'At'
   end
+  it 'returns the corrected single word (upcase)' do
+    expect(spellchecker('AT')).to eq 'AT'
+  end
 end
 
